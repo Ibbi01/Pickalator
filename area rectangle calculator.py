15d899e86ec007eaf6956463471a9113221496e3
Ibrahim A Siddique <ibrahimasiddique@Ibrahims-Air.home>
2026-09-04 17:58:45 +0100

    area of rectangle

---
+++ area rectangle calculator.py
@@ -0,0 +1,6 @@
+def total_area_of_rectangle():
+    length=float(input("enter length of rectangle"))
+    width=float(input("enter width of rectangle"))
+    total_area=length*width
+    print("total area of rectangle is",total_area)
+total_area_of_rectangle()
