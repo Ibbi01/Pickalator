637a9a5c1095f36417940109a76bd45c2944e0d6
Ibrahim A Siddique <ibrahimasiddique@Ibrahims-Air.home>
2026-09-04 17:33:49 +0100

    largest number of 3 values

---
+++ largest number out of 3 values.py
@@ -0,0 +1,9 @@
+def largest_num_out_of_3(num1,num2,num3):
+    if num1>num2 and num1>num3:
+        print(num1,"is biggest number")
+    elif num2>num1 and num2>num3:
+        print(num2,"is biggest number")
+    else:
+        print(num3,"is biggest number")
+largest_num_out_of_3(10,8,20)
+
