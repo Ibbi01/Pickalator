dcdec60709ddd011a256f0f89aa92991e226ef28
Ibrahim A Siddique <ibrahimasiddique@Ibrahims-Air.home>
2026-09-04 17:08:10 +0100

    python calculator program

---
+++ calculator.py
@@ -0,0 +1,15 @@
+def add (num1,num2):
+    print("sum is",num1+num2)
+def sub (num1,num2):
+    print("sub is",num1-num2)
+def mul (num1,num2):
+    print("mul is",num1*num2)
+def div (num1,num2):
+    print("div is",num1/num2)
+num1=int(input("enter a number"))
+num2=int(input("enter a number"))
+add(num1,num2)
+sub(num1,num2)
+mul(num1,num2)
+div(num1,num2)
+
