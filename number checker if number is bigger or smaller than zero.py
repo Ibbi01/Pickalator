d6cb2e891f5f34651c943ed284c31b6c3cb24ef9
Ibrahim A Siddique <ibrahimasiddique@Ibrahims-Air.home>
2026-09-06 19:26:12 +0100

    postive or negative checker

---
+++ number checker if number is bigger or smaller than zero.py
@@ -0,0 +1,9 @@
+def check_number():
+    check_number=int(input("Enter a number please"))
+    if check_number >0:
+        print("this is a postive number")
+    elif check_number == 0:
+        print("this is a zero lol")
+    else:
+        print("this is a negative number")
+check_number()
